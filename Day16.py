7583a98df95d2c71b37939b1edef055e6ef7fe12
Vanessa-Kris <nessakristo@gmail.com>
2021-09-27 12:29:00 +0100

    Day16

---
+++ Day16.py
@@ -0,0 +1,18 @@
+import math
+import world
+from world import europe
+from world.europe import norway
+import world.europe.spain
+from world.europe import norway
+from world.africa import zimbabwe
+
+# print(math.pi)
+# print(world)
+# print(world.africa)
+# print(europe)
+# print(europe.greece)
+# print(world.europe.norway)
+# print(world.europe.spain)
+# print(europe.spain)
+# print(norway)
+print(zimbabwe)
---
+++ world/europe/__init__.py
@@ -0,0 +1,3 @@
+# world/europe/__init__.py
+from . import greece
+from . import norway
